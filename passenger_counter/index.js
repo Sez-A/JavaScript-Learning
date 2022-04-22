618ceaa95f97e9d6f804b2e7a6bd9194689126e4
Sez-A <67644402+Sez-A@users.noreply.github.com>
2022-04-22 11:58:42 +0300

    More properly DOM manipulations

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -2,18 +2,18 @@ let count = 0;
 
 function increment() {
     count = count + 1;
-    document.getElementById("count-el").innerText = count;
+    document.getElementById("count-el").textContent = count;
 }
 
 function decrement() {
     count--;
-    document.getElementById("count-el").innerText = count;
+    document.getElementById("count-el").textContent = count;
 }
 
 function save() {
     let countWithDash = count + " - ";
     let saveEl = document.getElementById("save-el");
-    saveEl.innerText += " " + countWithDash;
+    saveEl.textContent += countWithDash;
 }
 
 
